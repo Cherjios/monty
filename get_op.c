66c08ee6f2d285c3ff2136f6ab0105feaa8568e4
Sergio Lopez <slopez0286@gmail.com>
2019-09-25 20:29:20 +0000

    Adding div mod mul

--- get_op.c
+++ get_op.c
@@ -7,33 +7,33 @@
  */
 void (*get_op(char *opcode))(stack_t **stack, unsigned int line_number)
 {
-        int i;
+	int i;
 
 	instruction_t get_op[] = {
 		{"push", _push},
 		{"pall", _pall},
-		{"pint", pint},
+		{"pint", _pint},
 		{"pop", _pop},
 		{"swap", _swap},
 		{"add", _add},
-/*		{"nop", _nop},
+		{"nop", _nop},
 		{"sub", _sub},
 		{"div", _div},
 		{"mul", _mul},
 		{"mod", _mod},
-		{"pchar", _pchar},
-		{"pstr", _pstr},
-		{"rotl", _rotl},
-		{"stack", stack},
+/**		{"pchar", _pchar},
+*		{"pstr", _pstr},
+*		{"rotl", _rotl},
+*		{"stack", stack},
 */		{NULL, NULL}
 	};
 
-        if (opcode == NULL)
-                return (NULL);
-        for (i = 0; get_op[i].opcode; i++)
-        {
-                if (strcmp(opcode, get_op[i].opcode) == 0)
-                        return (get_op[i].f);
-        }
-        return (NULL);
+	if (opcode == NULL)
+		return (NULL);
+	for (i = 0; get_op[i].opcode; i++)
+	{
+		if (strcmp(opcode, get_op[i].opcode) == 0)
+			return (get_op[i].f);
+	}
+	return (NULL);
 }
