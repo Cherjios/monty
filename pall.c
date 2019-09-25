d40d6fd7aed6688abe2398372b37a97e5ef3e917
Sergio Lopez <slopez0286@gmail.com>
2019-09-25 18:47:40 +0000

    Adding pall

--- pall.c
+++ pall.c
@@ -13,7 +13,7 @@ void _pall(stack_t **stack, unsigned int line_number)
 
 	while (*stack)
 	{
-		printf("%d\n", stack->n);
-		stack = stack->next;
+		printf("%d\n", (*stack)->n);
+		*stack = (*stack)->next;
 	}
 }
